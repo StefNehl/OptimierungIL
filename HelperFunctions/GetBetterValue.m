d6aecc0cbdc47b039fa623dfbd16b7a6434ecbfe
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2021-10-25 16:04:33 +0200

    Improved Helper functions

--- HelperFunctions/GetBetterValue.m
+++ HelperFunctions/GetBetterValue.m
@@ -5,16 +5,3 @@ function [betterValue] = GetBetterValue(x1, x2, maximum)
         betterValue = x2;
     end
 end
-
-%%
-function [result] = IsBetterValue(x1, x2, maximum)
-    if x1 > x2
-        result = x1;
-    else 
-        result = x2;
-    end
-
-    if ~maximum
-        result = ~result;
-    end
-end
