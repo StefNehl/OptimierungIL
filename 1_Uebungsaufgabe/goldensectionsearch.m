6665df44b50cfb5cf82399768f67a605f04a422b
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2021-10-26 12:26:30 +0200

    Finished golden selection search

--- 1_Uebungsaufgabe/goldensectionsearch.m
+++ 1_Uebungsaufgabe/goldensectionsearch.m
@@ -5,51 +5,31 @@ function [x] = goldensectionsearch(f, xu, xo, EPSILON, MAX_NUMBER_OF_ITERATIONS)
     count = 0;
     r = 0.6180339;
     findMaximum = true;
-    x = 10000000;
-    settingsData = SettingsData(r, EPSILON, findMaximum, MAX_NUMBER_OF_ITERATIONS);
+    settingsData = SettingsData(r, EPSILON, findMaximum, MAX_NUMBER_OF_ITERATIONS);     
     
-    x1 = xo - settingsData.R * (xo - xu);
-    x2 = xu + settingsData.R * (xo - xu);
-    
-    newParameterData = ParameterData(x1, x2, xu, xo);
-    
-    x = stepOne(newParameterData, count, settingsData);    
+    x = stepOne(f, xu, xo, count, settingsData);    
 end
 
-function [xOpt] = stepOne(parameterData, count, settingsData)       
+function [xOpt] = stepOne(f, xu, xo, count, settingsData)       
 
-    while count < settingsData.MAX_NUMBER_OF_ITERATIONS            
+    x1 = xo - settingsData.R * (xo - xu);    
+    x2 = xu + settingsData.R * (xo - xu);
     
-        xo = parameterData.Xo;
-        xu = parameterData.Xu;
-        
-        x1 = xo - settingsData.R * (xo - xu);
-        x2 = xu + settingsData.R * (xo - xu);
-
-        parameterData.X1 = x1;
-        parameterData.X2 = x2;
+    while count < settingsData.MAX_NUMBER_OF_ITERATIONS                                    
         
         if (xo - xu) <= settingsData.EPSILON
             [xOpt] = 0.5 * (xo + xu);
             break;
-        elseif IsBetterValue(x1, x2, settingsData.FindMaximum)            
-            parameterData = stepTwo(parameterData, settingsData.R);
+        elseif IsBetterValue(f(x1), f(x2), settingsData.FindMaximum)            
+            xo = x2;
+            x2 = x1;
+            x1 = xo - settingsData.R * (xo - xu);
         else
-            parameterData = stepThree(parameterData, settingsData.R);
+            xu = x1; 
+            x1 = x2; 
+            x2 = xu + settingsData.R * (xo - xu);  
         end
                
         count = count + 1;
     end     
-end
-
-function parameterData = stepTwo(parameterData, r)
-    parameterData.Xo = parameterData.X2;
-    parameterData.X2 = parameterData.X1;
-    parameterData.X1 = parameterData.Xo - r * (parameterData.Xo - parameterData.Xu);
-end
-
-function parameterData = stepThree(parameterData, r)
-    parameterData.Xu = parameterData.X1; 
-    parameterData.X1 = parameterData.X2; 
-    parameterData.X2 = parameterData.Xu + r * (parameterData.Xo - parameterData.Xu);  
 end
