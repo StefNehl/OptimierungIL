be8eb44a9e6ef00836999dfab0d5eac56c8ac5ff
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2022-01-31 14:48:18 +0100

    Implemented Solution with 1000 Items

--- 10_Uebung/KP/SolutionWithValue.java
+++ 10_Uebung/KP/SolutionWithValue.java
@@ -2,20 +2,43 @@ import java.util.ArrayList;
 
 public class SolutionWithValue
 {
-    private ArrayList<Integer> _itemIds;
+    private ArrayList<Integer> _newItemIds;
     private int _totalProfit;
     private int _totalWeight;
+    private int _size;
 
-    public SolutionWithValue(ArrayList<Integer> itemIds, int totalProfit, int totalWeight)
+    private  SolutionWithValue _oldSolution;
+
+    public SolutionWithValue(ArrayList<Integer> newItemIds, int newTotalProfit, int newTotalWeight, SolutionWithValue oldSolution)
     {
-        _itemIds = itemIds;
-        _totalProfit = totalProfit;
-        _totalWeight = totalWeight;
+        _newItemIds = newItemIds;
+        _oldSolution = oldSolution;
+        _totalProfit = newTotalProfit;
+        _totalWeight = newTotalWeight;
+        _size = newItemIds.size();
+
+        if(_oldSolution != null)
+        {
+            _totalProfit += _oldSolution.getTotalProfit();
+            _totalWeight += _oldSolution.getTotalWeight();
+            _size += _oldSolution.getSize();
+        }
     }
 
-    public ArrayList<Integer> getItemIds()
+    public boolean containsItem(Integer itemId)
     {
-        return _itemIds;
+        if(_newItemIds.contains(itemId))
+            return true;
+
+        if(_oldSolution == null)
+            return false;
+
+        return _oldSolution.containsItem(itemId);
+    }
+
+    public int getSize()
+    {
+        return _size;
     }
 
     public int getTotalProfit()
@@ -23,11 +46,25 @@ public class SolutionWithValue
         return _totalProfit;
     }
 
-    public int getTotalWeight(){ return _totalWeight; }
+    public int getTotalWeight()
+    {
+        return _totalWeight;
+    }
 
     public String toString()
     {
-        return "[" + _itemIds.size() + "/" + _totalProfit + "/" + _totalWeight + "]";
+        return "[" + getSize() + "/" + getTotalProfit() + "/" + getTotalWeight() + "]";
+    }
+
+    public ArrayList<Integer> cloneCurrentSolution()
+    {
+        var arrayList = new ArrayList<Integer>();
+
+        if(_oldSolution != null)
+            arrayList.addAll(_oldSolution.cloneCurrentSolution());
+
+        arrayList.addAll(_newItemIds);
+        return arrayList;
     }
 
     public String toString_1()
